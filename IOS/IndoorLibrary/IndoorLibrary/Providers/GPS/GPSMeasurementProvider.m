1d0a3863c63d8bf6d80a35f68262ccf3de336d0e
pischenkol <lina.pischenko@gmail.com>
2017-03-04 20:46:28 +0200

    added logic to GPSProvider

--- IOS/IndoorLibrary/IndoorLibrary/Providers/GPS/GPSMeasurementProvider.m
+++ IOS/IndoorLibrary/IndoorLibrary/Providers/GPS/GPSMeasurementProvider.m
@@ -29,21 +29,48 @@
     
     if (self != nil)
     {
-        //self.manager = [[CLLocationManager alloc] init];
-     
+        self.manager = [[CLLocationManager alloc] init];
     }
     return self;
 }
-
+- (void)locationManager:(CLLocationManager *)manager
+didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    switch (status) {
+        case kCLAuthorizationStatusNotDetermined:
+            [manager requestAlwaysAuthorization];
+            break;
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+            [manager startUpdatingLocation];
+            break;
+        case kCLAuthorizationStatusAuthorizedAlways:
+            [manager startUpdatingLocation];
+            break;
+        case kCLAuthorizationStatusRestricted:
+            [self _showAlert:@"App Permission Restricted. User can't enable Location Services"];
+            
+            break;
+            
+        default:
+            break;
+    }
+}
+-(void)_showAlert:(NSString*) message
+{
+    UIAlertView * alert = [[UIAlertView alloc]initWithTitle:@"" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    
+    [alert show];
+    
+}
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations{
     CLLocation *location = [locations objectAtIndex:0];
-    self.latitude = location.coordinate.latitude;
+    NSLog(@"%f", location.coordinate.latitude);
     self.longitude = location.coordinate.longitude;
     NSArray* coordinates = [[NSArray alloc] initWithObjects:@(location.coordinate.latitude), @(location.coordinate.longitude),nil];
     self.event = [[MeasurementEvent alloc] initWithTime:[[NSDate date] timeIntervalSince1970] withMeasurementType:GEO_VALUE andWithCordinate:coordinates];
     [MeasurementProvider deliver:self.event];
- 
-   
+    
+    
 }
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
     
@@ -53,12 +80,12 @@
         NSLog(@"Location Services Enabled");
         
         if([CLLocationManager authorizationStatus]==kCLAuthorizationStatusDenied){
-        UIAlertView    *alert = [[UIAlertView alloc] initWithTitle:
-                                              @"App Permission Denied"
-                                      message:@"To re-enable, please go to Settings and turn on Location Service for this app."
+            UIAlertView    *alert = [[UIAlertView alloc]
+                                     initWithTitle:@"App Permission Denied"
+                                     message:@"To re-enable, please go to Settings and turn on Location Service for this app."
                                      delegate:nil
-                            cancelButtonTitle:@"OK"
-                            otherButtonTitles:nil];
+                                     cancelButtonTitle:@"OK"
+                                     otherButtonTitles:nil];
             [alert show];
         }
     }
@@ -68,12 +95,19 @@
 {
     self.manager.delegate = self;
     self.manager.desiredAccuracy = kCLLocationAccuracyBest;
-
-        [self.manager requestWhenInUseAuthorization];
- 
-       
-  
-    [self.manager startUpdatingLocation];
+    [self.manager requestAlwaysAuthorization];
+    if ([self.manager locationServicesEnabled]){
+        
+        NSLog(@"Location Services Enabled");
+        NSLog(@"%d",[CLLocationManager authorizationStatus]);
+        
+        
+        [self.manager startUpdatingLocation];
+        
+        
+    }
+    
+    
 }
 -(void)stop
 {

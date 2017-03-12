7a5aa1d145b464abf4cbaf6d89642601be03f940
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-03-12 20:54:45 +0200

    Created BeaconProcessor

---
+++ NavigatorLib/navigator_lib/beacon/BeaconProcessor.h
@@ -0,0 +1,146 @@
+//
+// Created by  Oleksiy Grechnyev on 3/12/2017.
+//
+
+#pragma once
+
+#include <memory>
+
+#include "../filter/IFilter.h"
+#include "./Beacon.h"
+
+
+namespace navi {
+    namespace beacon {
+
+        /** @brief Calculates distance as a function of RSSI for a beacon
+         *
+         * @startuml
+         * class BeaconProcessor {
+         * // Beacon Processor //
+         * // Calculates distance as a function of RSSI for a beacon //
+         * // using both RSSI filter and distance filter //
+         * // Includes beacon data, and shared_ptr's to the filters //
+         * --
+         * - beacon : Beacon
+         * - rssiFilter : std::shared_ptr<IFilter>
+         * - distanceFilter : std::shared_ptr<IFilter>
+         * --
+         * + BeaconProcessor(beacon : const Beacon &,
+         *                  rssiFilter : const std::shared_ptr<IFilter> &,
+         *                  distanceFilter : const std::shared_ptr<IFilter> &)
+         * ..
+         * + process(rssi : double ) : double
+         * + reset() : void
+         * + reset(newBeacon : const Beacon &) : void
+         * ..
+         * + const getBeacon() : const Beacon &
+         * ..
+         * - const distanceFunction(rssi : double ) : double
+         * }
+         * @enduml
+         *
+         */
+        class BeaconProcessor {
+        public:
+            /** @brief Constructor
+             *
+             *  Note: While nullptr can be used to switch off a filter, it
+             *  is better to use std::make_shared<filter::NoFilter>()
+             *
+             * @param[in]  beacon           Beacon data (uid, location, txPower, damp)
+             * @param[in]  rssiFilter       RSSI filter (nullptr = for no filter)
+             * @param[in]  distanceFilter   distance filter (nullptr = for no filter)
+             */
+            BeaconProcessor(const Beacon &beacon, const std::shared_ptr<filter::IFilter> &rssiFilter,
+                            const std::shared_ptr<filter::IFilter> &distanceFilter) :
+                    beacon(beacon),
+                    rssiFilter(rssiFilter),
+                    distanceFilter(distanceFilter) {}
+
+
+            //---------------------------------------------
+            // Public methods
+            //---------------------------------------------
+
+            /** @brief Calculate distance from rssi using filters
+             *
+             * Both RSSI and distance filters are used if defined
+             *
+             * @param[in]   rssi    The input RSSI (in dBm or something like this)
+             * @return              The distance in meters
+             */
+            double process(double rssi)
+            {
+                double filteredRssi = rssi;
+
+                if (rssiFilter != nullptr) // Apply rssi filter if defined
+                    filteredRssi = rssiFilter -> process(filteredRssi);
+
+                // Calculate raw distance from the filtered RSSI
+                double distance = distanceFunction(filteredRssi);
+
+                if (distanceFilter != nullptr) // Apply distance filter if defined
+                    distance = distanceFilter -> process(distance);
+
+                return distance;
+            }
+
+            /// Reset the filters
+            void reset()
+            {
+                if (rssiFilter != nullptr) // Reset if not null
+                    rssiFilter -> reset();
+
+                if (distanceFilter != nullptr) // Reset if not null
+                    distanceFilter -> reset();
+            }
+
+            /// Reset the filters and update beacon data
+            void reset(const Beacon & newBeacon)
+            {
+                beacon = newBeacon; // Set new Beacon
+                reset(); // Reset the filters
+            }
+
+            const Beacon &getBeacon() const
+            {
+                return beacon;
+            }
+
+        private:
+            //---------------------------------------------
+            // Private methods
+            //---------------------------------------------
+
+            /** @brief The raw distance function, no filters
+             *
+             *  The formula is
+             *  distance = 10^((txpower-rssi) / (10*damp))
+             *
+             *  10 in the denominator comes from the definition of dB
+             *  The ideal (vacuum) damp is 2.0
+             *
+             * @param[in]   rssi   Input RSSI (in dBm or something like this)
+             * @return             Distance (im meters)
+             */
+            double distanceFunction(double rssi) const
+            {
+                return pow(10.0, (beacon.getTxPower() - rssi) / (10.0 * beacon.getDamp()));
+            }
+            //---------------------------------------------
+            // Private fields
+            //---------------------------------------------
+
+            /// The Beacon data object
+            Beacon beacon;
+
+            /// The rssi filter (1st filter)
+            std::shared_ptr<filter::IFilter> rssiFilter;
+
+            /// The distance filter (2nd filter)
+            std::shared_ptr<filter::IFilter> distanceFilter;
+        };
+
+    }
+}
--- NavigatorLib/navigator_lib/beacon/TrilatBeaconNavigator.h
+++ NavigatorLib/navigator_lib/beacon/TrilatBeaconNavigator.h
@@ -2,13 +2,21 @@
 // Created by  Oleksiy Grechnyev on 3/9/2017.
 //
 
+#include <map>
+#include <memory>
+
 #include "./AbstractBeaconNavigator.h"
+#include "./BeaconProcessor.h"
 
 #pragma once
 namespace navi {
     namespace beacon {
+
+        // TODO : implement this class
         class TrilatBeaconNavigator : public AbstractBeaconNavigator {
 
+        private:
+            std::map<BeaconUID, std::shared_ptr<BeaconProcessor>> beaconList;
         };
     }
 }

18e52a04401efe0b401362bb21f05c63b973c100
qyw <i.kyb@ya.ru>
2017-03-15 02:50:55 +0200

    Крупный рефакторинг, преорганизация файлов, ревью, небольшие правки по тексту. Часть1

---
+++ NavigatorLib/include/Navigator.h
@@ -0,0 +1,21 @@
+//
+// Created by kyb on 15.03.17.
+//
+
+#pragma once
+
+//TODO #include "./Navigator/dir.inc"
+#include "./Navigator/INavigator.h"
+#include "./Navigator/InputNavigationData.h"
+#include "./Navigator/OutputNavigationData.h"
+
+//#include "./Navigator/Beacons/dir.inc"
+//#include "./Navigator/Math/dir.inc"
+
+
+namespace Navigator {
+	
+	
+	
+	
+}
--- NavigatorLib/navigator_lib/README.md
+++
@@ -1,6 +0,0 @@
-Navigator Library, in progress
-
-This is a remake of tracking_lib
-
-Note: Doxyfile is the *Doxygen* configuration file. It requires *DOT\_PATH* and
-*PLANTUML\_JAR\_PATH* if you want to render UML diagrams.

877c63bf16a911c196480805966aaa31c93fbeec
pischenkol <lina.pischenko@gmail.com>
2017-03-31 00:04:07 +0300

    added bitcode for correct build

---
+++ IOS/IndoorLibrary/build/Intermediates/IndoorLibrary.build/Debug-iphoneos/IndoorLibrary.build/Objects-normal/armv7/AlphaBetaFilter.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/appleface/Documents/sdk/indoor-sdk/NavigatorLib/src/Navigator/Math/Filter/AlphaBetaFilter.cpp \
+  /Users/appleface/Documents/sdk/indoor-sdk/IOS/IndoorLibrary/IndoorLibrary/PrefixHeader.pch \
+  ../../NavigatorLib/include/Navigator/Math/Filter/AlphaBetaFilter.h \
+  ../../NavigatorLib/include/Navigator/Math/Filter/./IFilter.h

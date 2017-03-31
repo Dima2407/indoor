c8ae7da59b7c84924f86c972aa1f02e8b47b0a12
pischenkol <lina.pischenko@gmail.com>
2017-03-31 21:56:01 +0300

    added BeaconProcessor to build

--- IOS/IndoorLibrary/build/Intermediates/IndoorLibrary.build/Debug-iphoneos/IndoorLibrary.build/Objects-normal/armv7/BluetoothBridge.d
+++ IOS/IndoorLibrary/build/Intermediates/IndoorLibrary.build/Debug-iphoneos/IndoorLibrary.build/Objects-normal/armv7/BluetoothBridge.d
@@ -18,10 +18,15 @@ dependencies: \
   ../../NavigatorLib/include/Navigator/Beacons/./TrilatBeaconNavigator.h \
   ../../NavigatorLib/include/Navigator/Beacons/Factory/IFilterFactory.h \
   ../../NavigatorLib/include/Navigator/Math/Trilat/Trilat.h \
+  ../../NavigatorLib/include/Navigator/Math/Trilat/./TrilatRecord.h \
   ../../NavigatorLib/include/Navigator/Beacons/./Factory/AlphaBetaFilterFactory.h \
   ../../NavigatorLib/include/Navigator/Beacons/Factory/../../Math/Filter/AlphaBetaFilter.h \
   ../../NavigatorLib/include/Navigator/Beacons/./Factory/MovingAverageFilterFactory.h \
   ../../NavigatorLib/include/Navigator/Math/Filter/MovingAverageFilter.h \
   ../../NavigatorLib/include/Navigator/Math/Filter/./RingBuffer.h \
   ../../NavigatorLib/include/Navigator/Beacons/./Factory/NoFilterFactory.h \
+  ../../NavigatorLib/include/Navigator/Beacons/./Calibrate/BeaconCalibrator.h \
+  ../../NavigatorLib/include/Navigator/Beacons/Calibrate/./CalibrationConfig.h \
+  ../../NavigatorLib/include/Navigator/Beacons/Calibrate/./CalibrationPoint.h \
+  ../../NavigatorLib/include/Navigator/Beacons/./Calibrate/Algorithm/algorithm.h \
   ../../NavigatorLib/include/./Navigator/Math/dir.inc

058d0c9d7a019dbb682dc31e05cd48672ae53c0d
Victor Draban <drabanvictor@gmail.com>
2017-06-14 17:57:42 +0300

    creted tests for ToGlobal.class

--- NavigatorLib/unittest/Accel/ToGlobalTest.cpp
+++ NavigatorLib/unittest/Accel/ToGlobalTest.cpp
@@ -5,6 +5,8 @@
 #include "ToGlobalTest.h"
 #include "Navigator/Accel/ToGlobal.h"
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 
 namespace NaviTest {
     namespace Accel {
@@ -13,26 +15,57 @@ namespace NaviTest {
 
         void ToGlobalTest::testQuaternion() {
             constexpr double accuracy = 1.0e-10;
+            srand(time(0));
 
-//            double pitch = 0.2;
-//            double roll = 0.3;
-//            double yaw = 0.4;
-            Navigator::Accel::ToGlobal global;
-//            Eigen::Quaternion<double> q1 = global.initQuaternion(pitch, roll, yaw);
-            std::cout << " Hello world" << std::endl;
-            std::cout << CPPUNIT_ASSERT(true) << std::endl;
-            std::cout << CPPUNIT_ASSERT(false) << std::endl;
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
-//            CPPUNIT_ASSERT(global.initQuaternion(random(), random(), random()).norm() - 1 < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+            CPPUNIT_ASSERT(initQuaternion() < accuracy);
+
+            CPPUNIT_ASSERT(testAngleCorrectionPitch175() < accuracy);
+            CPPUNIT_ASSERT(testAngleCorrectionRoll175() < accuracy);
+            CPPUNIT_ASSERT(testAngleCorrectionYaw175() < accuracy);
+            CPPUNIT_ASSERT(testAngleCorrectionPitch38() < accuracy);
+            CPPUNIT_ASSERT(testAngleCorrectionRoll38() < accuracy);
+            CPPUNIT_ASSERT(testAngleCorrectionYaw38() < accuracy);
+        }
+
+        double ToGlobalTest::testAngleCorrectionPitch175() {
+            return fabs(175*(M_PI/180) - (3.054326190990));
+        }
+
+        double ToGlobalTest::testAngleCorrectionPitch38() {
+            return fabs(38*(M_PI/180) - (0.663225115757));
+        }
+
+        double ToGlobalTest::testAngleCorrectionRoll175() {
+            return fabs(175*(M_PI/180) - (3.054326190990));
+        }
+
+        double ToGlobalTest::testAngleCorrectionRoll38() {
+            return fabs(38*(M_PI/180) - (0.663225115757));
+        }
+
+        double ToGlobalTest::testAngleCorrectionYaw175() {
+            return fabs((-175-180)*(M_PI/180) - (-6.195918844579));
+        }
+
+        double ToGlobalTest::testAngleCorrectionYaw38() {
+            return fabs((-38-180)*(M_PI/180) - (-3.8048177693476));
         }
 
         double ToGlobalTest::random() {
-            return ((rand()%100)/100.0)*2*M_PI;
+            return (1.0*(rand()%1000)/1000)*2*M_PI;
+        }
+
+        double ToGlobalTest::initQuaternion() {
+            Navigator::Accel::ToGlobal global;
+            Eigen::Quaternion<double> result = global.initQuaternion(random(), random(), random());
+            return fabs(result.norm() - 1);
         }
     }
 }
--- NavigatorLib/unittest/Accel/ToGlobalTest.h
+++ NavigatorLib/unittest/Accel/ToGlobalTest.h
@@ -15,8 +15,15 @@ namespace NaviTest {
             CPPUNIT_TEST_SUITE_END();
         public:
             void testQuaternion();
+            double testAngleCorrectionPitch175();
+            double testAngleCorrectionRoll175();
+            double testAngleCorrectionYaw175();
+            double testAngleCorrectionPitch38();
+            double testAngleCorrectionRoll38();
+            double testAngleCorrectionYaw38();
         private:
             double random();
+            double initQuaternion();
         };
     }
 }

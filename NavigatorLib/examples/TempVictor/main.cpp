ddc9493af945c46b61974bcb4fd258dd34460684
Victor Draban <drabanvictor@gmail.com>
2017-06-21 15:47:14 +0300

    made an example about FilterImpl on updating data from Mathlab

--- NavigatorLib/examples/TempVictor/main.cpp
+++ NavigatorLib/examples/TempVictor/main.cpp
@@ -2,22 +2,48 @@
 // Created by  Victor Draban on 6/9/2017.
 //
 #include <iostream>
-#include "Navigator/Accel/ToGlobal.h"
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+#include "Navigator/Accel/FilterImpl.h"
+
 using namespace std;
 using namespace Navigator;
 using namespace Accel;
-using namespace Eigen;
 
-// Test Quaternion
+// Test Buttervord's filtr 8
 int main(){
+    vector<double> a;
+    vector<double> b;
+    double buff;
+    cout << fixed << setprecision(15);
+    ifstream finA("C:/QtProjects/to_victor/a.dat");
+    ifstream finB("C:/QtProjects/to_victor/b.dat");
 
-        double pitch = 0.2;
-        double roll = 0.3;
-        double yaw = 0.4;
-        ToGlobal global;
-        Quaternion<double> q1 = global.initQuaternion(pitch, roll, yaw);
-        cout << "it's normalize = " << q1.norm() << endl;
-        cout << "test" << endl;
+    while (finA >> buff) {
+        a.push_back(buff);
+    }
+    while (finB >> buff) {
+        b.push_back(buff);
+    }
+    finA.close();
+    finB.close();
 
-        return 0;
+    FilterImpl filter(9, a, b);
+    ifstream finX("C:/QtProjects/to_victor/tx.dat");
+    ofstream foutY("C:/QtProjects/to_victor/myTy.dat");
+    foutY << fixed << setprecision(15);
+    vector<Math::Filter::IFilter::Value> res;
+    while (finX >> buff) {
+        double timestamp = buff;
+        finX >> buff;
+        double val = buff;
+        Math::Filter::IFilter::Value myVal(val, timestamp);
+        Math::Filter::IFilter::Value result = filter.process(myVal);
+        foutY << "  " << result.timeStamp << "      " << result.val << endl;
+    }
+    foutY.close();
+    finX.close();
+    return 0;
 }

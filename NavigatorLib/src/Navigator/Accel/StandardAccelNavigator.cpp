2d8b181247d84a67805bfd4e858adb26c8104420
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-06-23 11:34:04 +0300

    change ay sign in StandardAccelNavigator

--- NavigatorLib/src/Navigator/Accel/StandardAccelNavigator.cpp
+++ NavigatorLib/src/Navigator/Accel/StandardAccelNavigator.cpp
@@ -7,9 +7,17 @@
 namespace Navigator {
     namespace Accel {
         Math::Position3D StandardAccelNavigator::process(const AccelReceivedData &ard) {
-            return trajectoryDetection.process(
-                    toGlobal.process(ard)
-            );
+            AccelOutputData aod = toGlobal.process(ard);
+
+            // We need this to use the stupid left-handed coordinates in this navigator
+            // We use the same coordinate system as in the beacon navigator (or, rather, meshgen2 and mobile apps)
+            // The (0,0) is the UPPER LEFT map corner and the y axis goes DOWN
+            // The trasformation to normal coordinates is
+            // y<->h-y , where h = map heightin meters, thus ay<->-ay, vy<->-vy
+            // I did it here so that the unit tests for toGlobal and trajectoryDetection wouldn't be affected
+            aod.ay = -aod.ay;
+
+            return trajectoryDetection.process(aod);
         }
     }
 }

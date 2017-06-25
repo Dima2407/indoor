b0f231c6e10dc7399f281b27bcf7328e201b8bd7
agrechnev <shrike4625@gmail.com>
2017-06-25 19:16:12 +0300

    logging in acceltester 1

--- NavigatorLib/tools/acceltester/main.cpp
+++ NavigatorLib/tools/acceltester/main.cpp
@@ -2,14 +2,18 @@
 // Created by Igor Maschikevich on 6/22/2017.
 //
 
+#include <cstdio>
+
 #include "Navigator.h"
 #include "rapidjson_min/include/rapidjson/document.h"
 #include "rapidjson_min/include/rapidjson/istreamwrapper.h"
 #include "myjson.h"
 using namespace std;
 using Navigator::Math::Position3D;
-using Navigator::Accel::StandardAccelNavigator;
-using Navigator::Accel::AccelReceivedData;
+// using Navigator::Accel::StandardAccelNavigator;
+// using Navigator::Accel::AccelReceivedData;
+
+using namespace Navigator::Accel;
 
 using namespace rapidjson;
 using namespace std;
@@ -33,9 +37,15 @@ int main() {
     d_angles.IsArray();
     d_accelerometer.IsArray();
 
-    StandardAccelNavigator standardAccelNavigator(nullptr, 0.5, 1.2, 50.0);
+    //StandardAccelNavigator standardAccelNavigator(nullptr, 0.5, 1.2, 20.0);
+    TrajectoryDetection traj(nullptr, 0.5, 1.2);
+    ToGlobal toGlob(20, false);
 
     tempOut << "p.x" << "      "  << "p.y" << "     "  << "p.z" << " "  << endl;
+    
+    FILE * fileLog1 = fopen("readLog.dat", "w");
+    fprintf(fileLog1, "   t  :  tStamp  iss:    ax         ay         az      :    pitch       roll       yaw \n");
+    
     for(int i = 0; i < d_angles.Size(); i++){
 
         const Value& for_angles = d_angles[i];
@@ -59,13 +69,27 @@ int main() {
         double aX = for_accelerometer["x"].GetDouble();
         double aY = for_accelerometer["y"].GetDouble();
         double aZ = for_accelerometer["z"].GetDouble();
-
+        
         AccelReceivedData ard{timestamp, aX, aY, aZ, pitch, azimuth, roll};
 
-        Position3D p = standardAccelNavigator.process(ard);
+        
+        
+        // Process by hand for debugging purpose
+        AccelOutputData aod = toGlob.process(ard);
+        aod.ay = -aod.ay;
+
+        Position3D p = traj.process(aod);
+                
+        // Normal way of processing
+        // Position3D p = standardAccelNavigator.process(ard);
+                
+        fprintf(fileLog1, "%5d : %10.6f %1d: ", i + 1, timestamp, aod.isStationary);
+        fprintf(fileLog1, "%10.6f %10.6f %10.6f : ", aX, aY, aZ);
+        fprintf(fileLog1, "%10.6f %10.6f %10.6f\n", pitch, roll, azimuth);
 
         tempOut << p.x << " "  << p.y << " "  << p.z << " "  << endl;
     }
+    fclose(fileLog1);
 
     return 0;
 }

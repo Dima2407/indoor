a91c983e40b0676e407c044944d2b1c832c521b5
Victor Draban <drabanvictor@gmail.com>
2017-07-27 15:14:35 +0300

    add functions: isInitialize, check last time that packet came

--- NavigatorLib/include/Navigator/Math/Kalman/KalmanFilter.h
+++ NavigatorLib/include/Navigator/Math/Kalman/KalmanFilter.h
@@ -12,27 +12,83 @@ namespace Navigator {
         namespace Kalman {
             class KalmanFilter: public Filter::IFilter {
 
+
             public:
                 KalmanFilter(const KalmanConfig& config = KalmanConfig()) :
                     config(config)
                 {}
 
+                /**
+                 * @brief KalmanFilter::process
+                 * @param in
+                 * @return
+                 * Application for Kalman's filter for raw RSSI
+                 * - smoothing of fluctuating RSSI values
+                 * - the restoration of "missing" packets with indications of the model Kalman filter
+                 */
                 Value process(Value in) override;
+
+                /**
+                 * @brief KalmanFilter::processOnlyPredict
+                 * @param timestamp
+                 * @return
+                 * Application for Kalman's filter without section "Correct"
+                 */
                 Value processOnlyPredict(double timestamp);
 
             private:
+
+                /**
+                *  @brief KalmanFilter::predictCurrentMoment
+                *  @param Ak
+                *  this method calculate: in predicting the values ​​of RSSIk and ΔRSSIk
+                *  for the current time moment k  from the filter model using the RSSI value,
+                *  the previous time point k-1
+                */
                 Eigen::Matrix<double, 2, 1> predictCurrentMoment(const Eigen::Matrix<double, 2, 2>& Ak);
+
+                /**
+                 * @brief KalmanFilter::predictError
+                 * @param Ak
+                 *  this method calculate: In the prediction of the value of the model error
+                 *  covariance matrix (result) using the matrix value for the previous
+                 *  time point k-1 and the noise matrix of the filter model (Q)
+                 */
                 Eigen::Matrix<double, 2, 2> predictError(const Eigen::Matrix<double, 2, 2>& Ak);
 
+                /**
+                 * @brief KalmanFilter::correctKalman
+                 * @param tempP
+                 * @return
+                 * calculate coefficient Kalmana
+                 */
                 Eigen::Matrix<double, 2, 1> correctKalman(const Eigen::Matrix<double, 2, 2>& tempP);
+
+                /**
+                 * @brief KalmanFilter::correctCurrentMoment
+                 * @param tempX
+                 * @param kalmansCoefficient
+                 * @param val
+                 * calculate values RSSI and ΔRSSI for current time
+                 * with given predictions
+                 */
                 void correctCurrentMoment(Eigen::Matrix<double, 2, 1> tempX,
                                                                  const Eigen::Matrix<double, 2, 1>& kalmansCoefficient,
                                                                  const double val);
+
+                /**
+                 * @brief KalmanFilter::correctError
+                 * @param tempP
+                 * @param kalmansCoefficient
+                 * calculate values of matrix the covariance of the errors model for current time
+                 */
                 void correctError(const Eigen::Matrix<double, 2, 2>& tempP,
                                                          const Eigen::Matrix<double, 2, 1>& kalmansCoefficient);
 
+                bool isInitialize = false;
                 KalmanConfig config;
-                double currentTime = 0;
+                double lastTime;
+                double lastPacketTime;
                 Eigen::Matrix<double, 2, 1> lastX;
                 Eigen::Matrix<double, 2, 2> lastP;
             };
--- NavigatorLib/src/Navigator/Math/Kalman/KalmanFilter.cpp
+++ NavigatorLib/src/Navigator/Math/Kalman/KalmanFilter.cpp
@@ -9,17 +9,18 @@ namespace Navigator {
     namespace Math {
         namespace Kalman {
 
-            /**
-             * @brief KalmanFilter::process
-             * @param in
-             * @return
-             * Application for Kalman's filter for raw RSSI
-             * - smoothing of fluctuating RSSI values
-             * - the restoration of "missing" packets with indications of the model Kalman filter
-             */
             Filter::IFilter::Value KalmanFilter::process(Filter::IFilter::Value in) {
                 double val = in.val;
-                double deltaT = in.timeStamp - currentTime;
+                lastPacketTime = in.timeStamp;
+                if (!isInitialize) {
+                    lastX(0,0) = in.val;
+                    lastX(1,0) = 0;
+                    lastP = config.P_INIT;
+                    lastTime = in.timeStamp;
+                    isInitialize = true;
+                    return in;
+                }
+                double deltaT = in.timeStamp - lastTime;
                 Eigen::Matrix<double, 2, 2> Ak;
                 Ak(0, 0) = 1;
                 Ak(0, 1) = deltaT;
@@ -32,18 +33,18 @@ namespace Navigator {
                 correctCurrentMoment(tempX, kalmansCoefficient, val);
                 correctError(tempP, kalmansCoefficient);
 
-                currentTime = in.timeStamp;
-                return Filter::IFilter::Value(lastX(0,0), currentTime);
+                lastTime = in.timeStamp;
+                return Filter::IFilter::Value(lastX(0,0), lastTime);
             }
 
-            /**
-             * @brief KalmanFilter::processOnlyPredict
-             * @param timestamp
-             * @return
-             * Application for Kalman's filter without section "Correct"
-             */
+            // -----------------
+
             Filter::IFilter::Value KalmanFilter::processOnlyPredict(double timestamp) {
-                double deltaT = timestamp - currentTime;
+                if (timestamp - lastPacketTime > config.timeout) {
+                    isInitialize = false;
+                    return Filter::IFilter::Value(lastX(0,0), timestamp);
+                }
+                double deltaT = timestamp - lastTime;
                 Eigen::Matrix<double, 2, 2> Ak;
                 Ak(0, 0) = 1;
                 Ak(0, 1) = deltaT;
@@ -53,63 +54,39 @@ namespace Navigator {
                 lastX = predictCurrentMoment(Ak);
                 lastP = predictError(Ak);
 
-                currentTime = timestamp;
-                return Filter::IFilter::Value(lastX(0,0), currentTime);
+                lastTime = timestamp;
+                return Filter::IFilter::Value(lastX(0,0), timestamp);
             }
 
-            /**
-            *  @brief KalmanFilter::predictCurrentMoment
-            *  @param Ak
-            *  this method calculate: in predicting the values ​​of RSSIk and ΔRSSIk
-            *  for the current time moment k  from the filter model using the RSSI value,
-            *  the previous time point k-1
-            */
+            // ------------ private -----------------
+
             Eigen::Matrix<double, 2, 1> KalmanFilter::predictCurrentMoment(const Eigen::Matrix<double, 2, 2>& Ak) {
                 return Ak * lastX;
             }
 
-            /**
-             * @brief KalmanFilter::predictError
-             * @param Ak
-             *  this method calculate: In the prediction of the value of the model error
-             *  covariance matrix (result) using the matrix value for the previous
-             *  time point k-1 and the noise matrix of the filter model (Q)
-             */
+            // -----------------
+
             Eigen::Matrix<double, 2, 2> KalmanFilter::predictError(const Eigen::Matrix<double, 2, 2>& Ak) {
                 return Ak * lastP * Ak.transpose() + config.Q;
             }
 
-            /**
-             * @brief KalmanFilter::correctKalman
-             * @param tempP
-             * @return
-             * calculate coefficient Kalmana
-             */
+            // -----------------
+
             Eigen::Matrix<double, 2, 1> KalmanFilter::correctKalman(const Eigen::Matrix<double, 2, 2>& tempP) {
                 double smallExpression = pow((config.H * tempP * config.H.transpose() + config.R), -1);
                 return tempP * config.H.transpose() * smallExpression;
             }
 
-            /**
-             * @brief KalmanFilter::correctCurrentMoment
-             * @param tempX
-             * @param kalmansCoefficient
-             * @param val
-             * calculate values RSSI and ΔRSSI for current time
-             * with given predictions
-             */
+            // -----------------
+
             void KalmanFilter::correctCurrentMoment(Eigen::Matrix<double, 2, 1> tempX,
                                                                            const Eigen::Matrix<double, 2, 1>& kalmansCoefficient,
                                                                            const double val) {
                 lastX = tempX + kalmansCoefficient * (val - config.H * lastX);
             }
 
-            /**
-             * @brief KalmanFilter::correctError
-             * @param tempP
-             * @param kalmansCoefficient
-             * calculate values of matrix the covariance of the errors model for current time
-             */
+            // -----------------
+
             void KalmanFilter::correctError(const Eigen::Matrix<double, 2, 2>& tempP,
                                             const Eigen::Matrix<double, 2, 1>& kalmansCoefficient) {
                 lastP = (config.I - kalmansCoefficient * config.H) * tempP;

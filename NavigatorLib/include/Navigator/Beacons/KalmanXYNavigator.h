36c2ef666de8f7a6efb085a368da95ff0e5971af
Victor Draban <drabanvictor@gmail.com>
2017-09-11 18:54:20 +0300

    start create arhitecture KalmanXYNavigator

--- NavigatorLib/include/Navigator/Beacons/KalmanXYNavigator.h
+++ NavigatorLib/include/Navigator/Beacons/KalmanXYNavigator.h
@@ -1,109 +1,27 @@
+//
+// Created by  Victor Draban on 9/11/2017.
+//
+
 #pragma once
-// By Oleksiy Grechnyev
 
+#include <vector>
 #include <memory>
-#include <unordered_map>
 
-#include "./AbstractBeaconNavigator.h"
-#include "./KalmanBeaconNavigatorConfig.h"
-#include "./KalmanBeaconProcessor.h"
-#include "./BeaconProcessor.h"
-#include "Navigator/Math/Kalman/KalmanConfig.h"
+#include "Navigator/Beacons/AbstractBeaconNavigator.h"
+#include "Navigator/Math/KalmanXY/KalmanXYFilter.h"
+#include "Navigator/Math/KalmanXY/KalmanXYConfig.h"
 #include "Navigator/Math/Position3D.h"
-#include "Navigator/Mesh/RectanMesh.h"
 
 namespace Navigator {
-namespace Beacons {
+    namespace Beacons {
+        class KalmanXYBeaconNavigator: public AbstractBeaconNavigator {
+        public:
 
-class KalmanBeaconNavigator : public AbstractBeaconNavigator
-{
-public: // ====== Methods
-    /// Constructor
-    KalmanBeaconNavigator(const std::shared_ptr<Mesh::RectanMesh> &mesh = nullptr,
-                          const KalmanBeaconNavigatorConfig & config = KalmanBeaconNavigatorConfig(),
-                          const Math::Kalman::KalmanConfig & filterConfig = Math::Kalman::KalmanConfig()
-            ) :
-        mesh(mesh),
-        config(config),
-        filterConfig(filterConfig) {}
-
-    /// Process a single input data -- not available
-    virtual const Math::Position3D &process(const BeaconReceivedData & brd) {
-        throw std::runtime_error("KalmanBeaconNavigator::process() with a singe packet is forbidden !");
+        private:
+//            std::vector<double> vec;
+            std::shared_ptr<AbstractBeaconNavigator> ptr;
+            Math::KalmanXY::KalmanXYFilter filter;
+            Math::Position3D lastPostion; // result
+        };
     }
-
-    /// Process a vector of input data, assuming identical timestamps
-    virtual const Math::Position3D &process(const std::vector<BeaconReceivedData> & brds) override;
-
-    /// Get last position
-    virtual const Math::Position3D &getLastPosition() const override{
-        return lastPosition;
-    }
-
-    /// Add a new beacon
-    virtual void addBeacon(const Beacon &beacon){
-        /// uid is the map key
-        beaconProcessorList[beacon.getUid()] = std::make_shared<KalmanBeaconProcessor>(beacon, filterConfig);
-    }
-
-    /// Find a BeaconProcessor by uid, nullptr if not found
-    virtual const std::shared_ptr<BeaconProcessor> findProcessorByUid(const BeaconUID & uid) const override;
-
-    /// Delete a beacon by uid
-    virtual void deleteBeacon(const BeaconUID &uid) override {
-        beaconProcessorList.erase(uid);
-    }
-
-    /// Delete all beacons and reset
-    virtual void clear() override{
-        beaconProcessorList.clear();
-        reset();
-    }
-
-    /// Reset filters etc but don't clear beacons
-    virtual void reset() override;
-
-    virtual const bool isInitFinished() const override {
-        return true;
-    }
-
-    const std::vector<BeaconUID> &getLastTrilatUids() const {
-        return lastTrilatUids;
-    }
-
-private: //========= Methods
-
-    /// Run the trilateration once based on current active beacons
-    void runTrilat();
-
-    /// Process a single input packet, no trilateration
-    void processPacket(const BeaconReceivedData &brd);
-
-    /// Check all beacons for the timeout using current timestamp
-    void checkTimeout(double timeStamp);
-
-    /// The post-processing with mesh and masktable
-    Math::Position3D postProcess(Math::Position3D pos);
-
-private: //========= Data
-    /// The Mesh + MaskTable
-    std::shared_ptr<Mesh::RectanMesh> mesh;
-
-    /// Config for these navigator
-    KalmanBeaconNavigatorConfig config;
-
-    /// Config for Kalman Filters
-    Math::Kalman::KalmanConfig filterConfig;
-
-    /// List of beacon processors for all beacons currently in use
-    std::unordered_map<BeaconUID, std::shared_ptr<KalmanBeaconProcessor>> beaconProcessorList;
-
-    /// Last located position
-    Math::Position3D lastPosition = Math::Position3D();
-
-    /// Beacons that took part in the latest trilateration
-    std::vector <BeaconUID> lastTrilatUids;
-};
-
-}
 }

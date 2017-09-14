c429a17fca4c804b898bf789e317a5de7cb40a17
Victor Draban <drabanvictor@gmail.com>
2017-09-14 15:09:25 +0300

    finish parse files from consumer

--- NavigatorLib/examples/testKalmanXYFilterByVictor/main.cpp
+++ NavigatorLib/examples/testKalmanXYFilterByVictor/main.cpp
@@ -5,6 +5,8 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <regex>
 
 #include "Navigator/Math/KalmanXY/KalmanXYConfig.h"
 #include "Navigator/Math/KalmanXY/KalmanXYFilter.h"
@@ -17,21 +19,78 @@ using namespace Math;
 int main() {
 
     vector<Position2D> vecXY;
-    vector<Position2D> vecPosition3Beacon;
+    vector<vector<Position2D>> vecPosition3Beacon;
     vector<Position2D> vecResult;
 
-    double buff;
+    string line;
     cout << fixed << setprecision(6);
+    regex rx("(\\d+\.\\d+)(\\s*)(\\d+\.\\d+)");
+    cmatch res;
 
-    ifstream fin("C:/QtProjects/testLogFiles/coordsINPUT.txt");
-    while (fin >> buff) {
-        vecTimestamp.push_back(buff);
-        fin >> buff;
-        vecVal.push_back(buff);
-        fin >> buff;
-        vecValResult.push_back(buff);
+// ------------- read coordsINPUT.txt
+//    ifstream fin("C:/QtProjects/testLogFiles/coordsINPUT.txt");
+//    getline(fin, line);
+
+//    while (getline(fin, line)) {
+//        regex_search(line.c_str(), res, rx);
+//        vecXY.push_back(Math::Position2D(stod(res[1]), stod(res[3])));
+//    }
+//    fin.close();
+// ------------- end read coordsINPUT.txt
+
+// ------------- check vecXY ----------
+//    cout << " vecXY containce : " << endl;
+//    for (Math::Position2D iter : vecXY) {
+//        cout << iter.x << " " << iter.y << endl;
+//    }
+// ------------- end check vecXY ----------
+
+
+// ------------- read beaconsINPUT.txt
+//    ifstream finBeacons("C:/QtProjects/testLogFiles/beaconsINPUT.txt");
+//    getline(finBeacons, line);
+
+//    while (getline(finBeacons, line)) {
+//        vector<Math::Position2D> tempVec;
+//        tempVec.reserve(3);
+//        while(regex_search(line.c_str(), rx)) {
+//            regex_search(line.c_str(), res, rx);
+//            tempVec.push_back(Math::Position2D(stod(res[1]), stod(res[3])));
+//            line = regex_replace(line.c_str(), rx, "!", regex_constants::format_first_only);
+//        }
+//        vecPosition3Beacon.push_back(tempVec);
+//        cout << endl;
+//    }
+//    finBeacons.close();
+// ------------- end read beaconsINPUT ----------
+
+// ------------- check vecPosition3Beacon ----------
+//        cout << " vecPosition3Beacon containce : " << endl;
+//        for (vector<Math::Position2D> iter : vecPosition3Beacon) {
+//            for (Math::Position2D innerIter : iter) {
+//                cout << innerIter.x << " " << innerIter.y << "\t";
+//            }
+//            cout << endl;
+//        }
+// ------------- end check vecPosition3Beacon ----------
+
+// ------------- read coordsOUTPUT.txt
+    ifstream finOut("C:/QtProjects/testLogFiles/coordsOUTPUT.txt");
+    getline(finOut, line);
+
+    while (getline(finOut, line)) {
+        regex_search(line.c_str(), res, rx);
+        vecResult.push_back(Math::Position2D(stod(res[1]), stod(res[3])));
     }
-    fin.close();
+    finOut.close();
+// ------------- end read coordsINPUT.txt
+
+// ------------- check vecResult ----------
+    cout << " vecResult containce : " << endl;
+    for (Math::Position2D iter : vecResult) {
+        cout << iter.x << " " << iter.y << endl;
+    }
+// ------------- end vecResult vecXY ----------
 
     KalmanXY::KalmanXYFilter filter;
 //    Position2D loc(1, 1);
